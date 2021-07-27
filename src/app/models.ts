59e84dd8bd5ea389836ad10ff908c3f8a9696406
Melheb younes <melheb.younes@gmail.com>
2021-07-27 17:34:03 +0200

    update tp

--- src/app/models.ts
+++ src/app/models.ts
@@ -10,10 +10,10 @@ export enum Avis {
 }
 
 export const mockCollegues: Collegue[] = [
-  {pseudo: 'Younes', score: 500, photoUrl:"https://randomuser.me/api/portraits/men/75.jpg"},
+  {pseudo: 'Younes', score: 999, photoUrl:"https://randomuser.me/api/portraits/men/75.jpg"},
   {pseudo: 'Younes1', score: 300, photoUrl:"https://randomuser.me/api/portraits/men/76.jpg"},
   {pseudo: 'Younes2', score: 400, photoUrl:"https://randomuser.me/api/portraits/men/77.jpg"},
   {pseudo: 'Younes3', score: 100, photoUrl:"https://randomuser.me/api/portraits/men/78.jpg"},
   {pseudo: 'Younes4', score: 150, photoUrl:"https://randomuser.me/api/portraits/men/79.jpg"},
-  {pseudo: 'Younes5', score: 22, photoUrl:"https://randomuser.me/api/portraits/men/45.jpg"}
+  {pseudo: 'Younes5', score: -999, photoUrl:"https://randomuser.me/api/portraits/men/45.jpg"}
 ]
